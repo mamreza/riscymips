fb373f6c2a3b7da8bda58f129d6906143884280a
kojot350 <kojot350@0989bf0a-c9f5-11dd-a880-a1081c7ac358>
2008-12-27 20:17:53 +0000

    Cleanup & comments.

--- lib/maindec.v
+++ lib/maindec.v
@@ -15,11 +15,29 @@ module maindec(input  [5:0] op,
   always @(*)
     case(op)
       6'b000000: controls = 9'b110000010; //Rtyp
-      6'b100011: controls = 9'b101001000; //LW
-      6'b101011: controls = 9'b001010000; //SW
-      6'b000100: controls = 9'b000100001; //BEQ
-      6'b001000: controls = 9'b101000000; //ADDI
+    //6'b000001: // BLTZ (rt = 0) or BGEZ (rt = 1)
       6'b000010: controls = 9'b000000100; //J
-      default:   controls = 9'bxxxxxxxxx; //???
+    //6'b000011: // JAL
+      6'b000100: controls = 9'b000100001; //BEQ
+    //6'b000101: // BNE
+    //6'b000110: // BLEZ
+    //6'b000111: // BGTZ
+      6'b001000: controls = 9'b101000000; //ADDI
+    //6'b001001: // ADDIU
+    //6'b001010: // SLTI
+    //6'b001011: // SLTIU
+    //6'b001100: // ANDI
+    //6'b001101: // ORI
+    //6'b001110: // XORI
+    //6'b001111: // LUI
+    //6'b100000: // LB
+    //6'b100001: // LH
+      6'b100011: controls = 9'b101001000; //LW
+    //6'b100100: // LBU
+    //6'b100101: // LHU
+    //6'b101000: // SB
+    //6'b101001: // SH
+      6'b101011: controls = 9'b001010000; //SW
+      default:   controls = 9'bxxxxxxxxx; //??? unknown instruction
     endcase
 endmodule
