8da3bc82b890ec76941a8df5d405484511f5a8c2
kojot350 <kojot350@0989bf0a-c9f5-11dd-a880-a1081c7ac358>
2008-12-28 00:02:36 +0000

    Alucont bus enhanced to 6 bit. New coments.

--- lib/mips.v
+++ lib/mips.v
@@ -15,7 +15,7 @@ module mips(input         clk, reset,
   wire        regdstE, alusrcE,
               pcsrcD,
               memtoregE, memtoregM, memtoregW, regwriteE, regwriteM, regwriteW;
-  wire [2:0]  alucontrolE;
+  wire [5:0]  alucontrolE;
   wire        flushE, equalD, branchD, jumpD;
 
   controller c(clk, reset, opD, functD, flushE, equalD,
@@ -37,29 +37,35 @@ module controller(input        clk, reset,
                   output       pcsrcD, branchD, alusrcE,
                   output       regdstE, regwriteE, regwriteM, regwriteW,
                   output       jumpD,
-                  output [2:0] alucontrolE);
+                  output [5:0] alucontrolE);
 
   wire [1:0] aluopD;
 
   wire       memtoregD, memwriteD, alusrcD,
              regdstD, regwriteD;
-  wire [2:0] alucontrolD;
+  wire [5:0] alucontrolD;
   wire       memwriteE;
 
+  // main decoder
   maindec md(opD, memtoregD, memwriteD, branchD,
              alusrcD, regdstD, regwriteD, jumpD,
              aluopD);
+  // alu decoder
   aludec  ad(functD, aluopD, alucontrolD);
 
+  // check for branch
   assign pcsrcD = branchD & equalD;
 
-  // pipeline registers
-  floprc #(8) regE(clk, reset, flushE,
+  // pipeline registers - control path
+  // register between Decode and Execute stages
+  floprc #(11) regE(clk, reset, flushE,
                   {memtoregD, memwriteD, alusrcD, regdstD, regwriteD, alucontrolD},
                   {memtoregE, memwriteE, alusrcE, regdstE, regwriteE,  alucontrolE});
+  // register between Execute and Memory stages
   flopr #(3) regM(clk, reset,
                   {memtoregE, memwriteE, regwriteE},
                   {memtoregM, memwriteM, regwriteM});
+  // register between Memory and Writeback stages
   flopr #(2) regW(clk, reset,
                   {memtoregM, regwriteM},
                   {memtoregW, regwriteW});
@@ -71,7 +77,7 @@ module datapath(input         clk, reset,
                 input         alusrcE, regdstE,
                 input         regwriteE, regwriteM, regwriteW,
                 input         jumpD,
-                input  [2:0]  alucontrolE,
+                input  [5:0]  alucontrolE,
                 output        equalD,
                 output [31:0] pcF,
                 input  [31:0] instrF,
@@ -94,7 +100,7 @@ module datapath(input         clk, reset,
   wire [31:0] aluoutE, aluoutW;
   wire [31:0] readdataW, resultW;
 
-  // hazard detection
+  // Hazard Detection Unit
   hazard    h(rsD, rtD, rsE, rtE, writeregE, writeregM, writeregW,
               regwriteE, regwriteM, regwriteW,
               memtoregE, memtoregM, branchD,
@@ -110,54 +116,80 @@ module datapath(input         clk, reset,
   regfile     rf(clk, regwriteW, rsD, rtD, writeregW,
                  resultW, srcaD, srcbD);
 
-  // Fetch stage logic
+  //-- Fetch stage logic ---
+  // F latch / PC register
   flopenr #(32) pcreg(clk, reset, ~stallF, pcnextFD, pcF);
+  // PC adder/incrementer, add 4 to PC
   adder       pcadd1(pcF, 32'b100, pcplus4F);
 
-  // Decode stage
-  flopenr #(32) r1D(clk, reset, ~stallD, pcplus4F, pcplus4D);
+  //-- Decode stage ---
+  // D latch / decode stage register (upper part)
   flopenrc #(32) r2D(clk, reset, ~stallD, flushD, instrF, instrD);
+  // D latch / decode stage register (lower part)
+  flopenr #(32) r1D(clk, reset, ~stallD, pcplus4F, pcplus4D);
+  // sign extend immediate value
   signext     se(instrD[15:0], signimmD);
+  // shift left immediate value by 2
   sl2         immsh(signimmD, signimmshD);
+  // add to to PC sign extended and shifted immediate value (branch)
   adder       pcadd2(pcplus4D, signimmshD, pcbranchD);
+  // forwarding multiplexers
   mux2 #(32)  forwardadmux(srcaD, aluoutM, forwardaD, srca2D);
   mux2 #(32)  forwardbdmux(srcbD, aluoutM, forwardbD, srcb2D);
+  // branch prediction comparator
   eqcmp       comp(srca2D, srcb2D, equalD);
 
+  // get operation code (6 bit) from instruction
   assign opD = instrD[31:26];
+  // get function code (6 bit) from instruction
   assign functD = instrD[5:0];
+  // get source register (5 bit) from instruction
   assign rsD = instrD[25:21];
+  // get target register (5 bit) from instruction
   assign rtD = instrD[20:16];
+  // get destination register (5 bit) from instruction
   assign rdD = instrD[15:11];
 
+  // flush D latch if branch or jump
   assign flushD = pcsrcD | jumpD;
+  //---
 
-  // Execute stage
+  //-- Execute stage ---
+  // latch E
   floprc #(32) r1E(clk, reset, flushE, srcaD, srcaE);
   floprc #(32) r2E(clk, reset, flushE, srcbD, srcbE);
   floprc #(32) r3E(clk, reset, flushE, signimmD, signimmE);
   floprc #(5)  r4E(clk, reset, flushE, rsD, rsE);
   floprc #(5)  r5E(clk, reset, flushE, rtD, rtE);
   floprc #(5)  r6E(clk, reset, flushE, rdD, rdE);
+  // forwarding multiplexers
   mux3 #(32)  forwardaemux(srcaE, resultW, aluoutM, forwardaE, srca2E);
   mux3 #(32)  forwardbemux(srcbE, resultW, aluoutM, forwardbE, srcb2E);
+  // ALU B operand source selector
   mux2 #(32)  srcbmux(srcb2E, signimmE, alusrcE, srcb3E);
+  // ALU Unit
   alu         alu(srca2E, srcb3E, alucontrolE, aluoutE);
+  // Write register selector (rt or td)
   mux2 #(5)   wrmux(rtE, rdE, regdstE, writeregE);
+  // ---
 
-  // Memory stage
+  //-- Memory stage ---
+  // latch M
   flopr #(32) r1M(clk, reset, srcb2E, writedataM);
   flopr #(32) r2M(clk, reset, aluoutE, aluoutM);
   flopr #(5)  r3M(clk, reset, writeregE, writeregM);
 
-  // Writeback stage
+  //-- Writeback stage ---
+  // latch W
   flopr #(32) r1W(clk, reset, aluoutM, aluoutW);
   flopr #(32) r2W(clk, reset, readdataM, readdataW);
   flopr #(5)  r3W(clk, reset, writeregM, writeregW);
+  // result selector (from ALU or Memory)
   mux2 #(32)  resmux(aluoutW, readdataW, memtoregW, resultW);
 
 endmodule
 
+// Hazard Unit
 module hazard(input  [4:0] rsD, rtD, rsE, rtE,
               input  [4:0] writeregE, writeregM, writeregW,
               input        regwriteE, regwriteM, regwriteW,
@@ -169,8 +201,8 @@ module hazard(input  [4:0] rsD, rtD, rsE, rtE,
   wire lwstallD, branchstallD;
 
   // forwarding sources to D stage (branch equality)
-  assign forwardaD = (rsD !=0 & rsD == writeregM & regwriteM);
-  assign forwardbD = (rtD !=0 & rtD == writeregM & regwriteM);
+  assign forwardaD = (rsD != 0 & rsD == writeregM & regwriteM);
+  assign forwardbD = (rtD != 0 & rtD == writeregM & regwriteM);
 
   // forwarding sources to E stage (ALU)
   always @(*)
@@ -198,18 +230,21 @@ module hazard(input  [4:0] rsD, rtD, rsE, rtE,
   // *** instead, another bypass network could be added from W to M
 endmodule
 
+// simple adder
 module adder(input  [31:0] a, b,
              output [31:0] y);
 
   assign #1 y = a + b;
 endmodule
 
+// simple comparator
 module eqcmp(input [31:0] a, b,
              output        eq);
 
   assign #1 eq = (a == b);
 endmodule
 
+// shift left by 2
 module sl2(input  [31:0] a,
            output [31:0] y);
 
@@ -217,6 +252,7 @@ module sl2(input  [31:0] a,
   assign #1 y = {a[29:0], 2'b00};
 endmodule
 
+// extend sign of immediate value to 32 bits
 module signext(input  [15:0] a,
                output [31:0] y);
 
@@ -233,6 +269,7 @@ module flopr #(parameter WIDTH = 8)
     else       q <= #1 d;
 endmodule
 
+// latch with clear
 module floprc #(parameter WIDTH = 8)
               (input                  clk, reset, clear,
                input      [WIDTH-1:0] d,
@@ -244,6 +281,7 @@ module floprc #(parameter WIDTH = 8)
     else            q <= #1 d;
 endmodule
 
+// latch with enable
 module flopenr #(parameter WIDTH = 8)
                 (input                  clk, reset,
                  input                  en,
@@ -255,6 +293,7 @@ module flopenr #(parameter WIDTH = 8)
     else if (en)    q <= #1 d;
 endmodule
 
+// latch with enable and clear
 module flopenrc #(parameter WIDTH = 8)
                  (input                  clk, reset,
                   input                  en, clear,
@@ -267,6 +306,7 @@ module flopenrc #(parameter WIDTH = 8)
     else if (en)    q <= #1 d;
 endmodule
 
+// 2 input multiplexer
 module mux2 #(parameter WIDTH = 8)
              (input  [WIDTH-1:0] d0, d1,
               input              s,
@@ -275,10 +315,20 @@ module mux2 #(parameter WIDTH = 8)
   assign #1 y = s ? d1 : d0;
 endmodule
 
+// 3 input multiplexer
 module mux3 #(parameter WIDTH = 8)
              (input  [WIDTH-1:0] d0, d1, d2,
               input  [1:0]       s,
               output [WIDTH-1:0] y);
 
   assign #1 y = s[1] ? d2 : (s[0] ? d1 : d0);
+endmodule
+
+// 4 input multiplexer
+module mux4 #(parameter WIDTH = 8)
+             (input  [WIDTH-1:0] d0, d1, d2, d3,
+              input  [2:0]       s,
+              output [WIDTH-1:0] y);
+
+  assign #1 y = s[2] ? d3 : (s[1] ? d2 : (s[0] ? d1 : d0));
 endmodule
